2243e602f399af735a2ad8a55bcc33a5561124d0
Ivan Bilyarov <3bilyi81@solent.ac.uk>
2019-09-27 10:56:28 +0100

    Second commit

--- Week1/Week1/src/test/java/NewEmptyJUnitTest.java
+++ Week1/Week1/src/test/java/NewEmptyJUnitTest.java
@@ -16,10 +16,28 @@ import static org.junit.Assert.*;
  * @author 3bilyi81
  */
 public class NewEmptyJUnitTest {
-    
+    String name = "Ivan";
+    int age = 20;
+    boolean isBirthday = true;
     @Test
       public void hello()  {
-      System.out.println("hello world");
+      
+      System.out.println("My name is Ivan");
+      System.out.println("________");
+      System.out.println("| o o |");
+      System.out.println("| --- | < Hello! " + name);
+      System.out.println("---------");
+      System.out.println(name);
+      System.out.println("My name is " + name + " and I am " + age + " years old!");
+      System.out.println("You are " + age + " years old!");
+      if (isBirthday = true){
+              age = age + 1;
+              System.out.println("Yay! It's your birthday! Now you are " + age);      
+      }
+      if (age >= 18) {
+      System.out.println("That means you’re old enough to drink!");
       }
  
+      }
+      
 }
